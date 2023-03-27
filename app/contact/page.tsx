8389c6922eba889fb0d01013d9239ca6b87a393a
Joel Torres <jotozhun@espol.edu.ec>
2023-03-26 23:16:29 -0500

    Created skills tab and contact

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -3,22 +3,26 @@ export default function Contact()
 {
     return (
         <div>
-            <h1 className="text-4xl font-bold text-center">Contact</h1>
-            <div className="mt-20">
-                <form className="flex justify-between flex-wrap space-y-2">
-                    <div>
-                    <input className="rounded-lg px-4 py-2 w-1/2" type="text" placeholder="Full name"/>
+            <h1 className="text-5xl font-bold text-center">Contact</h1>
+            <div className="mt-10">
+            <p className="text-4xl text-left font-bold mb-4">Send an email</p>
+                <form className="flex justify-between flex-wrap space-y-4">
+                    <div className="w-full flex flex-shrink space-x-4">
+                        <input className="rounded-lg px-4 py-2 w-1/2 shadow-md" type="text" placeholder="Full name"/>
+                        <input className="rounded-lg px-4 py-2 w-1/2 shadow-md" type="email" placeholder="Email"/>
                     </div>
-                    <div>
-                    <input className="rounded-lg px-4 py-2 w-1/2" type="email" placeholder="Email"/>
+                    <div className="w-full">
+                        <input className="rounded-lg px-4 py-2 w-full shadow-md" type="text" placeholder="Subject"/>
                     </div>
-                    <div>
-                    <input className="rounded-lg px-4 py-2 w-full" type="text" placeholder="Subject"/>
+                    <div className="w-full">
+                        <textarea   className="rounded-lg w-full px-4 py-2 shadow-md"
+                                    placeholder="Message"
+                                    style={{resize: "none"}}
+                                    ></textarea>
+                    </div >
+                    <div className="">
+                        <input className="w-28 py-2 text-white rounded-xl bg-blue-700 hover:cursor-pointer hover:bg-blue-600 hover:shadow-md" type="submit" value="Send Email"/>
                     </div>
-                    <div>
-                    <textarea className="rounded-lg" placeholder="Message"></textarea>
-                    </div>
-                    <input className="rounded-xl" type="submit" />
                 </form>
             </div>
         </div>
--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div>
       <div className="flex flex-wrap justify-between">
-        <div className="m-0 w-1/2">
+        <div className="m-0 w-full lg:w-1/2">
             <h1 className="text-7xl font-bold text-slate-900 leading-tight">Hey<br/>I'm Joel<br/>Software Developer</h1>
             <p className="mt-6 text-lg text-slate-500">I design, develop and maintain web applications in NextJS 13 and mostly with Python in the backend.
               My profile fits well with job positions like Full Stack Developer, Automation QA and DevOps with AWS.
@@ -14,9 +14,9 @@ export default function Home() {
               <button className="w-40 rounded-md py-2 px-4 bg-black text-white hover:text-yellow-400">See my projects</button>
             </div>
         </div>
-        <div className="m-0">
+        <div className="m-0 hidden lg:inline-block">
           <Image
-            className="rounded-full border-8 border-solid border-slate-50 hover:border-slate-300 hover:cursor-pointer"
+            className="rounded-full border-8 border-solid border-slate-50 hover:border-slate-300 hover:cursor-pointer md:flex-wrap-reverse"
             width={400}
             height={400}
             src="/joel_elegante.png"
